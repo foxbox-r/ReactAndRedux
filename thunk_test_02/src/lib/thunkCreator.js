da7cbd70b2f8c792770db3301f6e49c3abc4b1c7
rsua <foxboxr@gmail.com>
2020-10-08 02:55:29 +0900

    lib/thunkCreator.handleAsyncACtions 에 대한 커밋입니다.

--- thunk_test_02/src/lib/thunkCreator.js
+++ thunk_test_02/src/lib/thunkCreator.js
@@ -1,3 +1,5 @@
+import {reducerUtils} from "./reducerUtils"
+
 export const createPromiseThunk = (type,promiseCreator)=>{
     const [SUCCESS,ERROR] = [`${type}_SUCCESS`,`${type}_ERROR`]
     return param => async dispatch => {
@@ -14,6 +16,31 @@ export const createPromiseThunk = (type,promiseCreator)=>{
                 payload:e,
                 error:true,
             })
+        }
+    }
+}
+
+export const handlAsyncActions = (type,key)=>{
+    const [SUCCESS,ERROR] = [`${type}_SUCCESS`,`${type}_ERROR`]
+    return (state,action)=>{
+        switch(action.type){
+            case type:
+                return {
+                    ...state,
+                    [key]:reducerUtils.loading()
+                }
+            case SUCCESS:
+                return {
+                    ...state,
+                    [key]:reducerUtils.success(action.payload)
+                }
+            case ERROR:
+                return {
+                    ...state,
+                    [key]:reducerUtils.error(action.payload)
+                }
+            default:
+                return state;
         }
     }
 }
--- thunk_test_02/src/modules/PostsReducer.js
+++ thunk_test_02/src/modules/PostsReducer.js
@@ -1,6 +1,6 @@
 import * as postAPI from "../api/posts";
 import {reducerUtils} from "../lib/reducerUtils"
-import {createPromiseThunk} from "../lib/thunkCreator"
+import {createPromiseThunk,handlAsyncActions} from "../lib/thunkCreator"
 
 const initialState = {
     posts:reducerUtils.initial(),
@@ -18,38 +18,19 @@ const GET_POST_ERROR = "post/GET_POST_ERROR";
 export const getPosts = createPromiseThunk(GET_POSTS,postAPI.getPosts);
 export const getPostById = createPromiseThunk(GET_POST,postAPI.getPostById);
 
+const getPostsReducer = handlAsyncActions(GET_POSTS,"posts");
+const getPostReducer = handlAsyncActions(GET_POST,"post");
+
 export default function PostsReducer(state=initialState,action){
     switch(action.type){
         case GET_POSTS:
-            return {
-                ...state,
-                posts:reducerUtils.loading()
-            }
         case GET_POSTS_SUCCESS:
-            return {
-                ...state,
-                posts:reducerUtils.success(action.payload)
-            }
         case GET_POSTS_ERROR:
-            return {
-                ...state,
-                posts:reducerUtils.error(action.payload)
-            }
+            return getPostsReducer(state,action);
         case GET_POST:
-            return {
-                ...state,
-                post:reducerUtils.loading()
-            }
         case GET_POST_SUCCESS:
-            return {
-                ...state,
-                post:reducerUtils.success(action.payload)
-            }
         case GET_POST_ERROR:
-            return {
-                ...state,
-                post:reducerUtils.error(action.payload)
-            }
+            return getPostReducer(state,action);
         default:
             return state;
     }
